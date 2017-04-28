1e42d5ae88dc029b0173a868043c484178225a5f
CrystalFaith <crystallambert@Crystals-MacBook-Pro.local>
2017-04-28 08:47:43 -0700

    setup api route

---
+++ friendFinder/app/routing/apiRoutes.js
@@ -0,0 +1,61 @@
+
+  app.get("/api/friends", (req, res) => {
+      return res.json(friendCompare);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    var friendPush = req.body;
+    console.log(`params: ${req}`);
+    console.log(`friendPush: ${JSON.stringify(friendPush)}`);
+    var bestMatchTest = [];
+    friends.push(friendPush);
+    console.log(friends)
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    var characterScore = [];
+    var characterArray = [];
+    for(var i=0; i<friendCompare.length; i++){
+      var currentArrayName = friendCompare[i].friendName
+      var currentArrayURL = friendCompare[i].photoURL
+      var currentArray = friendCompare[i].questionArray
+      var scoreTest = [];
+      console.log(scoreTest)
+      for(var j=0; j<currentArray.length; j++){
+        var scoreish = currentArray[j] - friends[0].questionArray[j];
+        scoreTest.push(Math.abs(scoreish))
+
+      }
+      var sum = scoreTest.reduce(add, 0);
+
+      console.log(sum);
+      characterScore.push(sum)
+      characterArray.push({
+        name: currentArrayName,
+        url: currentArrayURL,
+        sum: sum,
+      })
+      console.log(scoreTest)
+    }
+    Array.min = function( array ){
+        return Math.min.apply( Math, array );
+    };
+    var minimum = Array.min(characterScore);
+    console.log(minimum)
+
+    for(var i=0; i<characterArray.length; i++){
+      if(characterArray[i].sum === minimum){
+        console.log(characterArray[i])
+        bestMatch = characterArray[i]
+        res.json(bestMatch);
+        // bestMatchTest.push(bestMatch);
+      }
+
+    }
+    friendCompare.push(friendPush);
+    console.log("friend compare "+friendCompare);
+    // console.log(`reservations: ${reservations}`);
+
+  });
--- friendFinder/server.js
+++ friendFinder/server.js
@@ -29,62 +29,68 @@ app.get("/survey", (req, res) => {
     return res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
-app.post("/api/friends", function(req, res) {
-  var friendPush = req.body;
-  console.log(`params: ${req}`);
-  console.log(`friendPush: ${JSON.stringify(friendPush)}`);
-  var bestMatchTest = [];
-  friends.push(friendPush);
-  console.log(friends)
 
-  function add(a, b) {
-      return a + b;
-  }
+  app.get("/api/friends", (req, res) => {
+      return res.json(friendCompare);
+  });
 
-  var characterScore = [];
-  var characterArray = [];
-  for(var i=0; i<friendCompare.length; i++){
-    var currentArrayName = friendCompare[i].friendName
-    var currentArrayURL = friendCompare[i].photoURL
-    var currentArray = friendCompare[i].questionArray
-    var scoreTest = [];
-    console.log(scoreTest)
-    for(var j=0; j<currentArray.length; j++){
-      var scoreish = currentArray[j] - friends[0].questionArray[j];
-      scoreTest.push(Math.abs(scoreish))
+  app.post("/api/friends", function(req, res) {
+    var friendPush = req.body;
+    console.log(`params: ${req}`);
+    console.log(`friendPush: ${JSON.stringify(friendPush)}`);
+    var bestMatchTest = [];
+    friends.push(friendPush);
+    console.log(friends)
 
-    }
-    var sum = scoreTest.reduce(add, 0);
-
-    console.log(sum);
-    characterScore.push(sum)
-    characterArray.push({
-      name: currentArrayName,
-      url: currentArrayURL,
-      sum: sum,
-    })
-    console.log(scoreTest)
-  }
-  Array.min = function( array ){
-      return Math.min.apply( Math, array );
-  };
-  var minimum = Array.min(characterScore);
-  console.log(minimum)
-
-  for(var i=0; i<characterArray.length; i++){
-    if(characterArray[i].sum === minimum){
-      console.log(characterArray[i])
-      bestMatch = characterArray[i]
-      res.json(bestMatch);
-      // bestMatchTest.push(bestMatch);
+    function add(a, b) {
+        return a + b;
     }
 
-  }
-  friendCompare.push(friendPush);
-  console.log("friend compare "+friendCompare);
-  // console.log(`reservations: ${reservations}`);
+    var characterScore = [];
+    var characterArray = [];
+    for(var i=0; i<friendCompare.length; i++){
+      var currentArrayName = friendCompare[i].friendName
+      var currentArrayURL = friendCompare[i].photoURL
+      var currentArray = friendCompare[i].questionArray
+      var scoreTest = [];
+      console.log(scoreTest)
+      for(var j=0; j<currentArray.length; j++){
+        var scoreish = currentArray[j] - friends[0].questionArray[j];
+        scoreTest.push(Math.abs(scoreish))
 
-});
+      }
+      var sum = scoreTest.reduce(add, 0);
+
+      console.log(sum);
+      characterScore.push(sum)
+      characterArray.push({
+        name: currentArrayName,
+        url: currentArrayURL,
+        sum: sum,
+      })
+      console.log(scoreTest)
+    }
+    Array.min = function( array ){
+        return Math.min.apply( Math, array );
+    };
+    var minimum = Array.min(characterScore);
+    console.log(minimum)
+
+    for(var i=0; i<characterArray.length; i++){
+      if(characterArray[i].sum === minimum){
+        console.log(characterArray[i])
+        bestMatch = characterArray[i]
+        res.json(bestMatch);
+        // bestMatchTest.push(bestMatch);
+      }
+
+    }
+    friendCompare.push(friendPush);
+    console.log("friend compare "+friendCompare);
+    // console.log(`reservations: ${reservations}`);
+
+  });
+
 
 
 app.use(express.static("app/public"));
