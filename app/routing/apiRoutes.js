b810a73b1cd5437d538a671a0805917676793065
CrystalFaith <crystallambert@Crystals-MacBook-Pro.local>
2017-04-28 11:45:41 -0700

    Experimental branch for Misha review/whatever

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+module.exports = app => {
+
+  app.get("/api/friends", (req, res) => {
+      return res.json(friendCompare);
+  });
+var friends = [];
+  app.post("/api/friends", function(req, res) {
+    var friendPush = req.body;
+    console.log(`params: ${req}`);
+    console.log(`friendPush: ${JSON.stringify(friendPush)}`);
+    var bestMatchTest = [];
+    friends.push(friendPush);
+    console.log(friends)
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    var characterScore = [];
+    var characterArray = [];
+    for(var i=0; i<friendCompare.length; i++){
+      var currentArrayName = friendCompare[i].friendName
+      var currentArrayURL = friendCompare[i].photoURL
+      var currentArray = friendCompare[i].questionArray
+      var scoreTest = [];
+      console.log(scoreTest)
+      for(var j=0; j<currentArray.length; j++){
+        var scoreish = currentArray[j] - friends[0].questionArray[j];
+        scoreTest.push(Math.abs(scoreish))
+
+      }
+      var sum = scoreTest.reduce(add, 0);
+
+      console.log(sum);
+      characterScore.push(sum)
+      characterArray.push({
+        name: currentArrayName,
+        url: currentArrayURL,
+        sum: sum,
+      })
+      console.log(scoreTest)
+    }
+    Array.min = function( array ){
+        return Math.min.apply( Math, array );
+    };
+    var minimum = Array.min(characterScore);
+    console.log(minimum)
+
+    for(var i=0; i<characterArray.length; i++){
+      if(characterArray[i].sum === minimum){
+        console.log(characterArray[i])
+        bestMatch = characterArray[i]
+        res.json(bestMatch);
+      }
+
+    }
+    friendCompare.push(friendPush);
+    console.log("friend compare "+friendCompare);
+
+  });
+
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+};
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var friendCompare = require("./app/data/friends.js")
 
 var app = express();
 var port = process.env.PORT || 8000
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 
 app.use(bodyParser.json());
@@ -18,75 +20,16 @@ app.use(bodyParser.json({
 }));
 
 
-var friends = [];
-
-app.get("/", function(req, res) {
-    return res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-
-app.get("/survey", (req, res) => {
-    return res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
 
 
-  app.get("/api/friends", (req, res) => {
-      return res.json(friendCompare);
-  });
+// app.get("/", function(req, res) {
+//     return res.sendFile(path.join(__dirname, "app/public/home.html"));
+// });
+//
+// app.get("/survey", (req, res) => {
+//     return res.sendFile(path.join(__dirname, "app/public/survey.html"));
+// });
 
-  app.post("/api/friends", function(req, res) {
-    var friendPush = req.body;
-    console.log(`params: ${req}`);
-    console.log(`friendPush: ${JSON.stringify(friendPush)}`);
-    var bestMatchTest = [];
-    friends.push(friendPush);
-    console.log(friends)
-
-    function add(a, b) {
-        return a + b;
-    }
-
-    var characterScore = [];
-    var characterArray = [];
-    for(var i=0; i<friendCompare.length; i++){
-      var currentArrayName = friendCompare[i].friendName
-      var currentArrayURL = friendCompare[i].photoURL
-      var currentArray = friendCompare[i].questionArray
-      var scoreTest = [];
-      console.log(scoreTest)
-      for(var j=0; j<currentArray.length; j++){
-        var scoreish = currentArray[j] - friends[0].questionArray[j];
-        scoreTest.push(Math.abs(scoreish))
-
-      }
-      var sum = scoreTest.reduce(add, 0);
-
-      console.log(sum);
-      characterScore.push(sum)
-      characterArray.push({
-        name: currentArrayName,
-        url: currentArrayURL,
-        sum: sum,
-      })
-      console.log(scoreTest)
-    }
-    Array.min = function( array ){
-        return Math.min.apply( Math, array );
-    };
-    var minimum = Array.min(characterScore);
-    console.log(minimum)
-
-    for(var i=0; i<characterArray.length; i++){
-      if(characterArray[i].sum === minimum){
-        console.log(characterArray[i])
-        bestMatch = characterArray[i]
-        res.json(bestMatch);
-      }
-
-    }
-    friendCompare.push(friendPush);
-    console.log("friend compare "+friendCompare);
-
-  });
 
 
 
